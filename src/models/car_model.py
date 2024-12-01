3eb4bd921f89399170bded3f3846ae95d4560511
VictoriiaFeshchenko <vikafeshch@gmail.com>
2024-12-01 19:11:51 +0200

    added lesson 29 as separate project

---
+++ src/models/car_model.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+@dataclass
+class Car:
+    """Car object for garage pages"""
+    brand: str = "Audi"
+    model: str = "TT"
+    mileage: float = 1000
---
+++ tests/test_instructions_page.py
@@ -0,0 +1,20 @@
+from src.models.car_model import Car
+from src.pages.instructions_page import InstructionsPage
+from src.utils.files_utils import FileUtils
+from tests.base_test import BaseTest
+from assertpy import assert_that
+
+class TestInstructionsPage(BaseTest):
+    """Test Instructions pages"""
+
+    def test_add_car(self, driver, auto_config):
+        # Login as guest
+        self.login_as_guest(driver, auto_config)
+        instructions_page = InstructionsPage(driver, auto_config)
+        instructions_page.open()
+        car = Car(brand="Audi", model="TT")
+        instruction_title = "Front windshield wipers on Audi TT"
+        # Click download
+        # instructions_page.click_download(instruction_title)
+        is_downloaded = FileUtils.wait_for_file_download(f"{instruction_title}.pdf")
+        assert_that(is_downloaded).is_true()
